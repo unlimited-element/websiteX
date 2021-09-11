83d1eaf412329dfd83f11f98320da7beb6501710
unlimited-element <73407996+unlimited-element@users.noreply.github.com>
2021-09-11 00:51:01 -0700

    Created Completed Project section

--- src/myComponent/projects.js
+++ src/myComponent/projects.js
@@ -22,55 +22,44 @@ class Projects extends Component {
         <div className="workchips">
           <Card className="workcard" data-aos="zoom-in-right">
             <CardContent className="work1">
-              <h1>Front End </h1>
+              <h1 className="center">Front End </h1>
             </CardContent>
             <Divider light />
             <CardContent>
-               <h2 style={{ color: "#cac531" }}>React.js</h2>
-              <Typography variant="body2" color="inherit" component="p">
-                Developed a full functional project for the MP Police . It is a
-                Crime Detection System which will identify the criminals in the
-                prone areas.
+               <h2 className="center" style={{ color: "#cac531"}}>ReactJS, AOS, Material UI</h2>
+              <Typography variant="body2" align="center" color="inherit" component="p">
+                The current project was constructed with tools such as React, Animate On Scroll, and Material UI to display understanding 
+                of current front end libraries and frameworks
               </Typography>
             </CardContent>
-            <CardActions>
-              <span>Jan 2019 -</span>-<span>May 2019</span>
-            </CardActions>
           </Card>
           <Card className="workcard" data-aos="zoom-in">
-            <CardContent className="work1">
-              <h1 style={{ color: "#f7f8f81" }}>Web Developer</h1>
-            </CardContent>{" "}
+            <CardContent className="center">
+              <h1> Back End </h1>
+            </CardContent>
             <Divider light />
             <CardContent>
-              {" "}
-              <i className="fas fa-map-marker-alt" />
-              <h2 style={{ color: "#cac531" }}>ACM Student Chapter</h2>
-              <Typography variant="body2" color="inherit">
-                Developed the official website for the event name AlgoRythm
-                conducted by <b> MITS ACM STUDENT CHAPTER</b>.
+
+              <h2 className="center" style={{ color: "#cac531" }}>NodeJS, ExpressJS </h2>
+              <Typography variant="body2" align="center" color="inherit">
+                Created an application with pure Node and Express frameworks to display backend competencies
               </Typography>
             </CardContent>
             <CardActions>
-              <span>Jan 2019 -</span>-<span>Feb 2019</span>
+
             </CardActions>
           </Card>
           <Card className="workcard" data-aos="zoom-in-left">
-            <CardContent className="work1">
-              <h1 style={{ color: "#f7f8f81" }}>Backend Developer</h1>
-            </CardContent>{" "}
+            <CardContent className="center">
+              <h1>Full Stack</h1>
+            </CardContent>
             <Divider light />
             <CardContent>
-              <i className="fas fa-map-marker-alt" />
-              <h2 style={{ color: "#cac531" }}>AgroGram</h2>
-              <Typography variant="body2" color="inherit" component="p">
-                Currently Working
+              <h2 className="center" style={{ color: "#cac531" }}>ReactJS NodeJS </h2>
+              <Typography variant="body2" color="inherit" align="center" component="p">
+                In Progress
               </Typography>
             </CardContent>
-            <CardActions>
-              <span>June 2019 -</span>
-              <span>Present</span>
-            </CardActions>
           </Card>
         </div>
       </Fragment>
